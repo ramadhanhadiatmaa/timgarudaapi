cea4d722f1b866010b6bd25ee68bf9c6a623cf5d
eventloungeid <eventloungeid@gmail.com>
2025-01-17 10:49:30 +0000

    feat(news): Add news to api

--- news/models/news_comment.go
+++ news/models/news_comment.go
@@ -5,9 +5,9 @@ import "time"
 type NewsComment struct {
 	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
 	PostID    int       `gorm:"not null" json:"post_id"`
-	PostInfo  News      `gorm:"foreignKey:PostID;references:ID" json:"post_info"`
+	PostInfo  News      `gorm:"foreignKey:ID;references:ID" json:"post_info"`
 	UserID    int       `gorm:"not null" json:"user_id"`
-	UserInfo  User      `gorm:"foreignKey:UserID;references:ID" json:"email_info"`
+	UserInfo  User      `gorm:"foreignKey:ID;references:ID" json:"email_info"`
 	Comment   string    `gorm:"type:varchar(250);not null" json:"comment"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
--- news/models/news_like.go
+++ news/models/news_like.go
@@ -5,9 +5,9 @@ import "time"
 type NewsLike struct {
 	ID        int       `gorm:"type:int(11);primaryKey;autoIncrement" json:"id"`
 	PostID    int       `gorm:"not null" json:"post_id"`
-	PostInfo  News      `gorm:"foreignKey:PostID;references:ID" json:"post_info"`
+	PostInfo  News      `gorm:"foreignKey:ID;references:ID" json:"post_info"`
 	UserID    int       `gorm:"not null" json:"user_id"`
-	UserInfo  User      `gorm:"foreignKey:UserID;references:ID" json:"email_info"`
+	UserInfo  User      `gorm:"foreignKey:ID;references:ID" json:"email_info"`
 	CreatedAt time.Time `json:"created_at"`
 }
 

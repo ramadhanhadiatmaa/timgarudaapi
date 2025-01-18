c14904f80d9eebc4be3f121538069d1d1e89ff45
eventloungeid <eventloungeid@gmail.com>
2025-01-18 23:36:25 +0000

    fix(auth): Data not showing

--- data/controllers/type_user_controller.go
+++ data/controllers/type_user_controller.go
@@ -2,39 +2,9 @@ package controllers
 
 import (
 	"data/models"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 )
-
-func ShowType(c *fiber.Ctx) error {
-	var type_user []models.TypeUser
-
-	if err := models.DB.Find(&type_user).Error; err != nil {
-		return jsonResponse(c, fiber.StatusInternalServerError, "Failed to load data", err.Error())
-	}
-
-	if len(type_user) == 0 {
-		return jsonResponse(c, fiber.StatusNotFound, "No data found", nil)
-	}
-
-	return c.JSON(type_user)
-}
-
-func IndexType(c *fiber.Ctx) error {
-	id := c.Params("id")
-	var type_user models.TypeUser
-
-	if err := models.DB.First(&type_user, "id = ?", id).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return jsonResponse(c, fiber.StatusNotFound, "No data found", nil)
-		}
-		return jsonResponse(c, fiber.StatusInternalServerError, "Failed to load data", err.Error())
-	}
-
-	return c.JSON(type_user)
-}
 
 func CreateType(c *fiber.Ctx) error {
 	var type_user models.TypeUser
@@ -48,38 +18,6 @@ func CreateType(c *fiber.Ctx) error {
 	}
 
 	return jsonResponse(c, fiber.StatusCreated, "Data successfully added", type_user)
-}
-
-func UpdateType(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil {
-		return jsonResponse(c, fiber.StatusBadRequest, "Invalid ID format", nil)
-	}
-
-	var type_user models.TypeUser
-	if err := models.DB.First(&type_user, id).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return jsonResponse(c, fiber.StatusNotFound, "No data found", nil)
-		}
-		return jsonResponse(c, fiber.StatusInternalServerError, "Failed to load data", err.Error())
-	}
-
-	var updateType models.TypeUser
-	if err := c.BodyParser(&updateType); err != nil {
-		return jsonResponse(c, fiber.StatusBadRequest, "Invalid input", err.Error())
-	}
-
-	if updateType.ID != 0 && updateType.ID != id {
-		if err := models.DB.First(&models.TypeUser{}, updateType.ID).Error; err == nil {
-			return jsonResponse(c, fiber.StatusBadRequest, "The updated ID is already in use", nil)
-		}
-	}
-
-	if err := models.DB.Model(&type_user).Updates(updateType).Error; err != nil {
-		return jsonResponse(c, fiber.StatusInternalServerError, "Failed to update data", err.Error())
-	}
-
-	return jsonResponse(c, fiber.StatusOK, "Data successfully updated", nil)
 }
 
 func DeleteType(c *fiber.Ctx) error {

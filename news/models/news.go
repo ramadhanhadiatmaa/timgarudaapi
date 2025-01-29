22efa11385059f10a10eb3e98f5f5c4bd68731bc
eventloungeid <eventloungeid@gmail.com>
2025-01-29 09:59:04 +0000

    fix(news): Change get data

--- news/models/news.go
+++ news/models/news.go
@@ -4,7 +4,7 @@ import "time"
 
 type News struct {
 	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	Content   string    `gorm:"type:varchar(250);not null" json:"content"`
+	Content   string    `gorm:"type:varchar(1000);not null" json:"content"`
 	Image     string    `gorm:"type:varchar(250);not null" json:"image"`
 	Category  int       `gorm:"type:int(11)" json:"category"`
 	CatInfo   string    `gorm:"column:category_name" json:"category_name"`

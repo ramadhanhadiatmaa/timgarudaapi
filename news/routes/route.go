f7fd21a307ac2703daa3c5e99ed316750685b3b5
eventloungeid <eventloungeid@gmail.com>
2025-01-29 10:10:58 +0000

    fix(news): Change get data

--- news/routes/route.go
+++ news/routes/route.go
@@ -8,34 +8,33 @@ import (
 )
 
 func Route(app *fiber.App) {
-	api := app.Group("/v1", middlewares.Auth)
-	api2 := app.Group("/v1")
+	api := app.Group("/v1")
 
-	cat := api.Group("/category")
+	cat := api.Group("/category", middlewares.Auth)
 	cat.Get("/", controllers.ShowCat)
 	cat.Get("/:id", controllers.IndexCat)
 	cat.Post("/", controllers.CreateCat)
 	cat.Put("/:id", controllers.UpdateCat)
 	cat.Delete("/:id", controllers.DeleteCat)
 
-	news := api.Group("/news")
+	news := api.Group("/news", middlewares.Auth)
 	news.Get("/:id", controllers.IndexNews)
 	news.Post("/", controllers.CreateNews)
 	news.Put("/upload/:id", controllers.UploadNewsImage)
 	news.Put("/:id", controllers.UpdateNews)
 	news.Delete("/:id", controllers.DeleteNews)
 
-	news2 := api2.Group("/news")
-	news2.Get("/", controllers.ShowNews)
+	publicNews := api.Group("/public/news")
+	publicNews.Get("/", controllers.ShowNews)
 
-	comment := api.Group("/comment")
+	comment := api.Group("/comment", middlewares.Auth)
 	comment.Get("/", controllers.ShowNewsCom)
 	comment.Get("/:id", controllers.IndexNewsCom)
 	comment.Post("/", controllers.CreateNewsCom)
 	comment.Put("/:id", controllers.UpdateNewsCom)
 	comment.Delete("/:id", controllers.DeleteNewsCom)
 
-	like := api.Group("/like")
+	like := api.Group("/like", middlewares.Auth)
 	like.Get("/", controllers.ShowNewsLike)
 	like.Get("/:id", controllers.IndexNewsLike)
 	like.Post("/", controllers.CreateNewsLike)

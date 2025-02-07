5439c084402c59a24f3eee047dcede5f0c359b6f
eventloungeid <eventloungeid@gmail.com>
2025-02-07 10:45:46 +0000

    feat(schedule): Adding schedule to api

--- schedule/routes/route.go
+++ schedule/routes/route.go
@@ -2,17 +2,22 @@ package routes
 
 import (
 	"schedule/controllers"
-	"schedule/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Route(app *fiber.App) {
-	api := app.Group("/v1", middlewares.Auth)
+	api := app.Group("/v1" /* , middlewares.Auth */)
 
 	garuda := api.Group("/garuda")
 	garuda.Post("/", controllers.Create)
 	garuda.Get("/:id", controllers.Index)
 	garuda.Put("/:id", controllers.Update)
 	garuda.Delete("/:id", controllers.Delete)
+
+	team := api.Group("/team")
+	team.Post("/", controllers.CreateTeam)
+	team.Get("/:id", controllers.IndexTeam)
+	team.Put("/:id", controllers.UpdateTeam)
+	team.Delete("/:id", controllers.DeleteTeam)
 }
